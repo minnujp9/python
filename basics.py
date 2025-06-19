a1dc2951f8ac0cfbac8bad57d7ca1e5e5c0f4238
minnu <minnujayaprakash7@gmail.com>
2025-06-19 15:39:31 +0530

    initialised

---
+++ basics.py
@@ -0,0 +1,88 @@
+# x = 5
+# x = 10
+# print(x)
+
+# a = int(input("Enter a number:"))
+# b = int(input("Enter another number:"))
+# print("sum =",a+b)
+#
+# a = "banana"
+# print(a[1:5])
+#
+# a = 3
+#
+# if a%2==0 or a>0:
+#     print("even")
+# else:
+#     print("odd")
+
+# i =1
+# while i<=10:
+#     print(i)
+#     i=i+1
+
+# for x in range(1,6):
+#     if x == 3:
+#         continue
+#     print(x)
+
+#LIST
+
+# a = ['apple','kiwi','banana','pineapple','apple']
+# b = ['strawberry','blueberry']
+#to change an element in a list
+# a[0]='orange'
+
+#slicing in list
+# print(a[1:4])
+
+# to add element in a list
+# a.append('orange')
+# a.insert(1,'orange')
+
+#to remove an element in a list
+# a.remove('pineapple')
+# a.pop(1)
+# print(a)
+
+#to print each element one by one
+# for x in a:
+#     print(x)
+
+#to join two list
+# print(a+b)
+
+
+#TUPLE
+
+# a = ('banana','kiwi','orange','apple','banana')
+# b = list(a)
+# print(b)
+# a=tuple(b)
+# print(a)
+
+#SET
+
+# a = {'banana','kiwi','orange','apple','banana'}
+# a.add('strawberry')
+# a.remove('orange')
+# a.pop()
+# print(a)
+
+#DICTIONARY
+
+# a = {'brand':'maruti','color':'red','price':4500000}
+# a['brand']='toyota' # to change the existing value
+# a['model']='swift'  #to add a new pair
+# a.pop('brand')  # to remove an item
+# a.popitem() # to remove the last item
+# del a['brand'] # to remove an item
+# print(a)
+
+#FUNCTION
+
+# def my_function(a,b):
+#     pass
+#
+# print(my_function(b=2,a=3))
+# print(my_function(4,3))
---
+++ basicsroughwork.py
@@ -0,0 +1,25 @@
+#print("Hello")
+
+
+#a=10
+#b=5
+#s=a*b
+#print(s)
+
+#a=2
+#if a==1:
+  #  print("one")
+#else:
+  #  print("Two")
+
+#for x in range(3):
+ #   print("sum")
+  #  if x==1:
+   #     print("divide")
+
+a="Minnu"
+print(a[-3:-1])
+
+
+
+
